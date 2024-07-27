f00dd35ba01f5e4c68c69f7922b26a6f11834eae
Ankita <sehrawatankita1@gmail.com>
2024-07-27 15:46:30 +0530

    helper and icon file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Portfolio from "./Components/Portfolio";
 
 function App() {
   return (
     <div>
       <Header />
+      <Portfolio/>
     </div>
   );
 }
---
+++ src/Components/Common/Icons.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export function Red_dot_icon() {
+    return (
+        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.5 21C16.299 21 21 16.299 21 10.5C21 4.70101 16.299 0 10.5 0C4.70101 0 0 4.70101 0 10.5C0 16.299 4.70101 21 10.5 21Z" fill="#FF3344" />
+        </svg>
+
+    )
+}
+export function Green_dot_icon() {
+    return (
+        <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M17.5 35C27.165 35 35 27.165 35 17.5C35 7.83502 27.165 0 17.5 0C7.83502 0 0 7.83502 0 17.5C0 27.165 7.83502 35 17.5 35Z" fill="#2FEFB1"/>
+        </svg>
+        
+    )
+}
+export function Blue_dot_icon() {
+    return (
+        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.5 21C16.299 21 21 16.299 21 10.5C21 4.70101 16.299 0 10.5 0C4.70101 0 0 4.70101 0 10.5C0 16.299 4.70101 21 10.5 21Z" fill="#4857EC"/>
+        </svg>        
+    )
+}

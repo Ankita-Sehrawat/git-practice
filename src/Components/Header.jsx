36d7822b4dd84232df2fb12389d394230ba37306
Ankita <sehrawatankita1@gmail.com>
2024-07-27 11:19:14 +0530

    'header'

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quod assumenda accusamus quam dolor adipisci commodi, eius, rerum ratione recusandae alias tenetur laborum aliquid praesentium fugit iusto iure quos asperiores.
+    </div>
+  )
+}
+
+export default Header

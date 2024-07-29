51780406ab64ff7bb1240b904d3545c2632505dd
subham <subhamsankhla1122@gmail.com>
2024-07-29 11:22:23 +0530

    meet team

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-<<<<<<< HEAD
+
 import './App.css';
 import MeetOurTeam from './Components/MeetOurTeam';
 import Header from './Components/Header'
-=======
-import "./App.css";
-import Header from "./Components/Header";
 import Portfolio from "./Components/Portfolio";
->>>>>>> f00dd35ba01f5e4c68c69f7922b26a6f11834eae
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
+      <Portfolio />
       <MeetOurTeam />
+      {/* <Footer/> */}
     </>
-=======
-      <Portfolio/>
-    </div>
->>>>>>> f00dd35ba01f5e4c68c69f7922b26a6f11834eae
   );
 }
 export default App;

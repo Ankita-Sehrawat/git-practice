9b9e2dbde5b95ca5a55d1dbfcffd646099edb499
Ankita <sehrawatankita1@gmail.com>
2024-08-01 09:57:33 +0530

    footer updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,19 +7,21 @@ const Footer = () => {
   return (
     <div className="bg-[#ffffff] lg:pt-[145px] pt-[50px] pb-[50px] lg:pb-[88px] relative">
       <div className="xl:max-w-[1140px] mx-auto xl:px-3 container px-6">
-        <div className="flex flex-row flex-wrap mb-[62px]">
-          <div className="xl:w-4/12 px-3 w-full flex flex-col justify-center sm:justify-start items-center sm:items-start">
+        <div className="flex flex-row flex-wrap  mb-[62px]">
+          <div className=" w-full lg:w-4/12 px-3 flex flex-col justify-center sm:justify-start items-center sm:items-start  ">
             <img src={footlogo} alt="footlogo" className="cursor-pointer" />
-            <p className="text-[#AAB2C8] text-[400]  text-center sm:text-start leading-[24px] text-[16px] lg:text-[20px] lg:max-w-[369px] pt-6">
+            <p className="text-[#AAB2C8] font-[400] leading-[24px] text-[16px] lg:text-[20px] lg:max-w-[369px] pt-6 text-center lg:text-start">
               Beneath the surface of every groundbreaking application lies a foundation built on
               countless lines of code, each meticulously crafted to achieve perfection.
             </p>
-            <div className="flex gap-5 mt-[32px] mb-[30px] lg:mb-[0px]">
+            <div className="flex gap-5 w-full justify-center  my-4 sm:my-[10px] lg:mb-[0px] lg:mt-8">
               <div className="social_hover cursor-pointer ">
                 <Net_icon />
               </div>
               <div className="social_hover cursor-pointer ">
+                <a href="https://facebook.com/">
                 <Facebook_icon />
+                </a>
               </div>
               <div className="social_hover cursor-pointer ">
                 <Tweeter_icon />
@@ -29,35 +31,35 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="xl:w-8/12 px-3 md:pt-10 xl:pt-[0px] w-full ">
+          <div className="w-full lg:w-8/12 px-3 pt-7 lg:pt-[0px] ">
             <div className="flex flex-row flex-wrap">
-              <div className="md:w-3/12 w-1/2 flex justify-center sm:justify-start xl:justify-end">
+              <div className="sm:w-3/12 w-1/2 flex justify-center lg:justify-end">
                 <ul>
-                  <li className="text-[#000000] font-bold text-[18px] lg:text-[24px] leading-[29px] mb-[27px] ">
+                  <li className="text-[#000000] font-bold text-[18px] lg:text-[24px] leading-[29px] mb-2 lg:mb-[27px] text-center sm:text-start ">
                     Home
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start  ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Partners
                     </a>
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Features
                     </a>
-                  </li>{' '}
-                  <li className="mb-[19px]">
+                  </li>
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Testimonial
                     </a>
-                  </li>{' '}
-                  <li className="mb-[19px]">
+                  </li>
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
@@ -66,33 +68,33 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
-              <div className="md:w-3/12 w-1/2 flex justify-center sm:justify-start xl:justify-end">
+              <div className="sm:w-3/12 w-1/2 flex justify-center lg:justify-end">
                 <ul>
-                  <li className="text-[#000000] font-bold  text-[18px] lg:text-[24px] leading-[29px] mb-[27px] ">
+                  <li className="text-[#000000] font-bold  text-[18px] lg:text-[24px] leading-[29px] mb-2 lg:mb-[27px] text-center sm:text-start ">
                     Pages
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       About Us
                     </a>
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Careers
                     </a>
-                  </li>{' '}
-                  <li className="mb-[19px]">
+                  </li>
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Case Studies
                     </a>
-                  </li>{' '}
-                  <li className="mb-[19px]">
+                  </li>
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
@@ -101,59 +103,59 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
-              <div className="md:w-3/12 w-1/2 flex justify-center sm:justify-start xl:justify-end">
+              <div className="sm:w-3/12 w-1/2 flex justify-center lg:justify-end mt-5 sm:mt-0">
                 <ul>
-                  <li className="text-[#000000] font-bold  text-[18px] lg:text-[24px] leading-[29px] mb-[27px] ">
+                  <li className="text-[#000000] font-bold  text-[18px] lg:text-[24px] leading-[29px] mb-2 lg:mb-[27px] text-center sm:text-start ">
                     Blog
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Blog Listing
                     </a>
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Blog Article
                     </a>
-                  </li>{' '}
-                  <li className="mb-[19px]">
+                  </li>
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Newsroom
                     </a>
-                  </li>{' '}
+                  </li>
                 </ul>
               </div>
-              <div className="md:w-3/12 w-1/2 flex justify-center sm:justify-start xl:justify-end">
+              <div className="sm:w-3/12 w-1/2 flex justify-center lg:justify-end mt-5 sm:mt-0">
                 <ul>
-                  <li className="text-[#000000] font-bold  text-[18px] lg:text-[24px] leading-[29px] mb-[27px] ">
+                  <li className="text-[#000000] font-bold  text-[18px] lg:text-[24px] leading-[29px] mb-2 lg:mb-[27px] text-center sm:text-start ">
                     Portfolio
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Portfolio
                     </a>
                   </li>
-                  <li className="mb-[19px]">
+                  <li className="mb-[10px] lg:mb-[19px] text-center sm:text-start ">
                     <a
                       href=""
                       className="text-[#AAB2C8] text-[600] text-[16px] lg:text-[20px] leading-[29px] hover:text-[#000] transition-all duration-500 ">
                       Single Case
                     </a>
-                  </li>{' '}
+                  </li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-        <p className=" text-[#808080] text-[500] text-[18px] leading-[22px]  text-center sm:text-start">
+        <p className=" text-[#808080] font-[500] text-[18px] leading-[22px]  text-center lg:text-start">
           © {year} Game Analytics
         </p>
       </div>
